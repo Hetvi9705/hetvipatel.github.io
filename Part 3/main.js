7a9bd2fceafc624edd8d86528348f58026aa25bd
Hetvi9705 <hetvipiyushkumar.patel@dcmail.ca>
2024-10-29 01:21:28 -0400

    collision Detect
    Add a collisionDetect method to the Ball class

--- Part 3/main.js
+++ Part 3/main.js
@@ -71,3 +71,16 @@ function loop() {
     requestAnimationFrame(loop);
 }
 loop();
+collisionDetect() {
+    for (const ball of balls) {
+        if (this !== ball) {
+            const dx = this.x - ball.x;
+            const dy = this.y - ball.y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+
+            if (distance < this.size + ball.size) {
+                ball.color = this.color = randomRGB();
+            }
+        }
+    }
+}
